56954378f1bd84b63e6d317926f85fe9fb695b12
BHFoamer17 <bharris1@chicagobooth.edu>
2015-07-30 16:19:42 -0500

    block changes - to table and photo name

--- app/models/industry_type.rb
+++ app/models/industry_type.rb
@@ -1,5 +1,15 @@
 class IndustryType < ActiveRecord::Base
 
-has_many :service_providers
+  has_many :service_providers
 
+  PHOTO_VIEW = ["Architect", "Contractor", "Interior Designer"]
+  TABLE_VIEW = ["Attorney", "Brokerage", "Furniture Vendor", "Moving Company", "Office Equipment", "Telecommunications"]
+
+  def displays_as_photo_view?
+    PHOTO_VIEW.includes? self.name
+  end
+
+  def displays_as_table_view?
+    TABLE_VIEW.includes? self.name
+  end
 end
